915fc1cc63e9b4070dbe57035c6c52051f9d1ae6
crazyhottommy <tangming2005@gmail.com>
2022-08-20 02:01:29 -0400

    change blog name

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,13 +1,13 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Jay Doe';
+    : 'Ming Tommy Tang';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : 'cell line to command line';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'All rights reserved.';
+    : 'blog https://divingintogeneticsandgenomics.rbind.io/';
 
   return {
     name,
